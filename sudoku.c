77974e2a68fda59dfa741fe72d38e0dc8a2c5b75
Replit user <>
2023-05-03 05:08:38 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -51,8 +51,8 @@ int is_valid(Node* n){
 
 List* get_adj_nodes(Node* n){
   List* list=createList();
-  int auxFila = -1;
-  int auxCol = -1;
+  int auxFila = 0;
+  int auxCol = 0;
   
   for (int i = 0 ; i < 9 ; i++) {
     for (int j = 0 ; j < 9 ; j++) {
