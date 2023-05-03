e32bf53bff929f44ebaeac4f1cbf9b0dfead812e
Replit user <>
2023-05-03 15:46:42 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -69,7 +69,22 @@ int is_valid(Node* n){
       }
     }
   }
-  
+
+  for(int i = 0 ; i < 9 ; i+=3) {
+    for(int j = 0 ; i < 9 ; j+=3) {
+      array = (int*) calloc(sizeof(int),10);
+      if (array == NULL) exit(EXIT_FAILURE);
+      for(int p = i ; p < i + 3 ; p++) {
+        for(int l = j ; l < j + 3 ; l++) {
+          if(array[n->sudo[p][l]] == 1) {
+            return 0;
+          } else {
+            array[n->sudo[p][l]] = 1;
+          }
+        }
+      }
+    }
+  }
   
   
   return 1;
